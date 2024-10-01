7d4af55f8428fd0409855ca79c88cb2ccfd85002
eve <eevvee@tuta.io>
2024-10-01 06:25:45 +0100

    Linux: Update version for elfs plugin as changing from task.pid to task.tgid in the output

--- volatility3/framework/plugins/linux/elfs.py
+++ volatility3/framework/plugins/linux/elfs.py
@@ -25,7 +25,7 @@ class Elfs(plugins.PluginInterface):
     """Lists all memory mapped ELF files for all processes."""
 
     _required_framework_version = (2, 0, 0)
-    _version = (2, 0, 2)
+    _version = (2, 1, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
