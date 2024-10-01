54dbda4ada991382c56a320c82b853a3d818a045
eve <eevvee@tuta.io>
2024-10-01 06:26:50 +0100

    Linux: Update version for lsof plugin as changing from task.pid to task.tgid in the output

--- volatility3/framework/plugins/linux/lsof.py
+++ volatility3/framework/plugins/linux/lsof.py
@@ -21,7 +21,7 @@ class Lsof(plugins.PluginInterface, timeliner.TimeLinerInterface):
     """Lists open files for each processes."""
 
     _required_framework_version = (2, 0, 0)
-    _version = (1, 2, 0)
+    _version = (1, 3, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
