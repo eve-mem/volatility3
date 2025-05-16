e9679c1b45f6271bc113bcc422d59c7d3f3c7066
eve <eevvee@tuta.io>
2025-05-16 17:34:13 +0100

    Linux: bash add missing VersionRequirement for psscan

--- volatility3/framework/plugins/linux/bash.py
+++ volatility3/framework/plugins/linux/bash.py
@@ -41,6 +41,9 @@ class Bash(plugins.PluginInterface, timeliner.TimeLinerInterface):
                 name="pslist", component=pslist.PsList, version=(4, 0, 0)
             ),
             requirements.VersionRequirement(
+                name="psscan", component=psscan.PsScan, version=(2, 0, 0)
+            ),
+            requirements.VersionRequirement(
                 name="timeliner",
                 component=timeliner.TimeLinerInterface,
                 version=(1, 0, 0),
